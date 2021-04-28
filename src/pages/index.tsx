2a1dfef8107fa596b37140a33c261d6067ca0017
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-04-28 17:58:53 -0300

    feat: terminal component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "../components/Header";
+
+import { Header, Terminal } from "../components";
 
 export default function Index() {
     return (
         <div>
             <Header />
+            <Terminal />
         </div>
     );
 }
